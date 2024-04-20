4931ea54e5cf115cf14b2bd2227fb4d6f05d55a0
oljh <oljh@itu.dk>
2024-04-20 11:08:43 +0200

    movement physics

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
     private Vector2 _moveInput;
     private float _rotateInput;
     private bool _boosting;
-
     private bool _holdingMirror;
+    private Rigidbody2D _rigidbody2D;
+
 
     public float MoveSpeed = 10f;
     public float RotationSpeed = 100f;
@@ -19,10 +20,16 @@ public class Movement : MonoBehaviour
 
     public GameObject Mirror;
 
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     void ToggleMirror()
     {
         _holdingMirror = !_holdingMirror;
         Mirror.SetActive(_holdingMirror);
+        _rigidbody2D.velocity = Vector2.zero;
     }
 
     void OnMove(InputValue value)
@@ -45,17 +52,21 @@ public class Movement : MonoBehaviour
         ToggleMirror();
     }
 
+    void FixedUpdate()
+    {
+        if (!_holdingMirror)
+            Move();
+    }
+
     void Update()
     {
         if (_holdingMirror)
             Rotate();
-        else
-            Move();
     }
 
     void Move()
     {
-        transform.position += MoveSpeed * Time.deltaTime * (Vector3)_moveInput;
+        _rigidbody2D.velocity = _moveInput * MoveSpeed;
     }
 
     void Rotate()
@@ -63,5 +74,4 @@ public class Movement : MonoBehaviour
         var rotationSpeed = _boosting ? BoostRotationSpeed : RotationSpeed;
         transform.Rotate(0, 0, -_rotateInput * rotationSpeed * Time.deltaTime);
     }
-
 }
