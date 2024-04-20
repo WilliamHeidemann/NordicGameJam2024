aae476f5e0d4b1fc582da293d1baac995f298b51
William Skou Heidemann <Williamskou2227@gmail.com>
2024-04-20 22:53:16 +0200

    automatic level advance

--- Assets/Scripts/Bonfire.cs
+++ Assets/Scripts/Bonfire.cs
@@ -2,18 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bonfire : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-
+    private static int currentLevel = 0;
+    private bool hasBeenLit = false;
+    
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public void LightUp()
+    public async void LightUp()
     {
+        if (hasBeenLit) return;
+        hasBeenLit = true;
         spriteRenderer.color = Color.red;
+        // Play happy animation
+        await Awaitable.WaitForSecondsAsync(3f);
+        SceneManager.LoadScene(++currentLevel);
     }
 }
