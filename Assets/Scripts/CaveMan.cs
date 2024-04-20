48689b03b1ede80c619541c816617cfaf0bc42a2
William Skou Heidemann <Williamskou2227@gmail.com>
2024-04-20 20:12:37 +0200

    caveman and level

---
+++ Assets/Scripts/CaveMan.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CaveMan : MonoBehaviour
+{
+    public async void Die()
+    {
+        await Awaitable.WaitForSecondsAsync(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
