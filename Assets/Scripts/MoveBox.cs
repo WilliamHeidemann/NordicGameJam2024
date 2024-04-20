6c945bdc1ec753b3999fd53c3e930c102104584a
William Skou Heidemann <Williamskou2227@gmail.com>
2024-04-20 18:11:50 +0200

    fix movable

--- Assets/Scripts/MoveBox.cs
+++ Assets/Scripts/MoveBox.cs
@@ -10,31 +10,29 @@ public class MoveBox : MonoBehaviour
 {
     public float PushingForce = .25f;
     public Vector2 LastMovedDirection;
-    public float MaxDistance = 1f;
+    private float MaxDistance = 3f;
+    public List<Movable> movables;
 
-    public HashSet<(float, Moveable)> moveables = new();
+    private void Start()
+    {
+        movables = FindObjectsByType<Movable>(FindObjectsSortMode.None).ToList();
+    }
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.P) && LastMovedDirection != Vector2.zero) // && wasd bliver brugt
+        print(movables.Count);
+        if (Input.GetKey(KeyCode.P) && LastMovedDirection != Vector2.zero)
         {
-            if (!moveables.Any()) return;
-            var (distance, movable) = moveables.ToList().OrderBy(pair => pair.Item1).First();
+            if (!movables.Any()) return;
+            var closest = movables.OrderBy(movable => Vector3.Distance(movable.transform.position, transform.position)).First();
+            var distance = Vector3.Distance(closest.transform.position, transform.position);
+            if (distance > MaxDistance) return;
             if (distance < 0.5f) return;
-            movable.transform.position = Vector2.MoveTowards(movable.transform.position, transform.position, 0.2f);
+            closest.transform.position = Vector2.MoveTowards(closest.transform.position, transform.position, 0.2f);
+            print("Moving");
         }
     }
-
-    public void AddToMoveables(float distance, Moveable moveable)
-    {
-        moveables.Add((distance, moveable));
-    }
-
-    public void RemoveFromMoveables(float distance, Moveable moveable)
-    {
-        moveables.Remove((distance, moveable));
-    }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Pushable"))
